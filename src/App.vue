dcb3daf619218ceb8821276ad82760b57a7f0376
wangchao <wangxiaochaocccc@163.com>
2022-11-09 14:26:57 +0800

    columnlist基本构建

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,40 @@
 <template>
-  <div>
-    <img alt="Vue logo" src="./assets/logo.png"/>
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="container">
+    <column-list :column="columnData" />
   </div>
- 
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import { defineComponent } from "vue";
+import "bootstrap/dist/css/bootstrap.min.css";
+import ColumnList, { columnType } from "./components/ColumnList.vue";
 
 export default defineComponent({
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
+    ColumnList,
+  },
+  setup() {
+    const columnData: columnType[] = [
+      {
+        id: 1,
+        title: "test1的专栏",
+        description: "这是的test1专栏，有一段非常有意思的简介，可以更新一下欧",
+        avatar:
+          "http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100",
+      },
+      {
+        id: 2,
+        title: "test2的专栏",
+        description: "这是的test2专栏，有一段非常有意思的简介，可以更新一下欧",
+        avatar:
+          "http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100",
+      },
+    ];
+    return {
+      columnData,
+    };
+  },
 });
 </script>
 
---
+++ src/components/ColumnList.vue
@@ -0,0 +1,31 @@
+<template>
+  <ul>
+    <li v-for="item in column" :key="item.id">
+      <img :src="item.avatar" alt="" />
+      <div class="title">{{ item.title }}</div>
+      <div class="desc">{{ item.description }}</div>
+      <button>进入专栏</button>
+    </li>
+  </ul>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from "vue";
+
+export interface columnType {
+  id: number;
+  avatar: string;
+  title: string;
+  description: string;
+}
+export default defineComponent({
+  name: "ColumnList",
+  props: {
+    column: {
+      type: Array as PropType<columnType[]>,
+      required: true,
+    },
+  },
+});
+</script>
+<style scoped></style>
