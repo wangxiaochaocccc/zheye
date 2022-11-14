87030163be8dcfd26b14ff81f7e876cbeddfed36
wangchao <wangxiaochaocccc@163.com>
2022-11-14 22:31:43 +0800

    feat:header完成

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,24 @@
 <template>
+  <global-header :user="store.state" />
   <router-view />
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import GlobalHeader, { UserType } from "@/components/GlobalHeader.vue";
+import { useStore } from "vuex";
+export default defineComponent({
+  name: "app",
+  components: {
+    GlobalHeader,
+  },
+  setup() {
+    const store = useStore<UserType>();
+
+    return {
+      store,
+    };
+  },
+});
+</script>
+<style scoped></style>
