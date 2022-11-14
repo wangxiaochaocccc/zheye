f5e621ec91a227581d2bb6e4d24196c980f98e1b
wangchao <wangxiaochaocccc@163.com>
2022-11-14 12:02:11 +0800

    feat:项目页面基本结构views

---
+++ src/views/Login.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="">登录</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "testVue",
+});
+</script>
+<style scoped></style>
